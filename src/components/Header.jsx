02918c1f99c0f3b4e256470eaa72119ee74e4c3b
k3st <kiddoturtle04@gmail.com>
2024-08-13 22:50:58 +0800

    resolve import error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,9 @@ const Header = () => {
         <nav className="hidden fixed top-[5rem] left-0 bottom-0 right-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent ">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
-              <a></a>
+              <a key={item.id} href={item.url}>
+                {item.title}
+              </a>
             ))}
           </div>
         </nav>
