cfdba908ffa17a840baedc8bc0ab53959e7a49a8
k3st <kiddoturtle04@gmail.com>
2024-08-14 21:08:04 +0800

    draft hero using section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
 import Section from "./Section";
 
 const Hero = () => {
-  return <Section></Section>;
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      Hero
+    </Section>
+  );
 };
 
 export default Hero;
