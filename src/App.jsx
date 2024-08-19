e35cee366a97c7a7469f1c3778f927fe08e8453c
k3st <kiddoturtle04@gmail.com>
2024-08-20 03:45:18 +0800

    create pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Services from "./components/Services";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 const App = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
