861c5d1de02918560e8c559b339c69f5f16c6c0a
k3st <kiddoturtle04@gmail.com>
2024-08-21 03:26:09 +0800

    complete roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,6 +3,8 @@ import Heading from "./Heading";
 import { roadmap } from "../constants";
 import { check, check2, grid, loading1 } from "../assets";
 import Tagline from "./Tagline";
+import Gradient from "./design/Roadmap";
+import Button from "./Button";
 
 const Roadmap = () => {
   return (
@@ -16,51 +18,57 @@ const Roadmap = () => {
 
             return (
               <div
-                key={item.id}
                 className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
                   item.colorful ? "bg-conic-gradient" : "bg-n-6"
                 }`}
+                key={item.id}
               >
-                <div className="relative p-8 bg-n-8 rounded-[2rem] overflow-hidden xl:p-15">
-                  <div className=" absolute top-0 left-0 max-w-full">
+                <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
+                  <div className="absolute top-0 left-0 max-w-full">
                     <img
-                      src={grid}
                       className="w-full"
+                      src={grid}
                       width={550}
                       height={550}
                       alt="Grid"
                     />
                   </div>
-                  <div className="relatize z-1">
+                  <div className="relative z-1">
                     <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
                       <Tagline>{item.date}</Tagline>
-                      <div className=" flex items-center py-1 px-4 bg-n-1 rounded-none text-n-8">
+
+                      <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
                         <img
-                          src={item.status === "done" ? check2 : loading1}
                           className="mr-2.5"
+                          src={item.status === "done" ? check2 : loading1}
                           width={16}
                           height={16}
-                          alt="Status"
+                          alt={status}
                         />
                         <div className="tagline">{status}</div>
                       </div>
-
-                      <div className="mb-10 -my-10 -mx-15">
-                        <img
-                          src={item.imageUrl}
-                          height={420}
-                          width={630}
-                          alt={item.title}
-                        />
-                      </div>
-                      <h4 className="h4 mb-4"> {item.title}</h4>
-                      <p className="body-2 text-n-4">{item.text}</p>
                     </div>
+
+                    <div className="mb-10 -my-10 -mx-15">
+                      <img
+                        className="w-full"
+                        src={item.imageUrl}
+                        width={628}
+                        height={426}
+                        alt={item.title}
+                      />
+                    </div>
+                    <h4 className="h4 mb-4">{item.title}</h4>
+                    <p className="body-2 text-n-4">{item.text}</p>
                   </div>
                 </div>
               </div>
             );
           })}
+          <Gradient />
+        </div>
+        <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
+          <Button href="/roadmap">Our roadmap</Button>
         </div>
       </div>
     </Section>
