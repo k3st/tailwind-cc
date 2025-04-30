f0781c810211b518c3ae30eb7ebe074abea41d64
k3st <kiddoturtle04@gmail.com>
2025-04-30 09:56:35 +0800

    fixing browser router for github page deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <Router basename="/tailwind-cc">
       <App />
     </Router>
   </StrictMode>
