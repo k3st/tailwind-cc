fc055107788f3ac2f825ff78986e3086c1502422
k3st <kiddoturtle04@gmail.com>
2025-04-30 11:01:04 +0800

    Revert "complete footer"
    This reverts commit da5fdd1c039219d3e2a95a3b73caad0ec2ca59d5.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,7 @@
 import React from "react";
-import Section from "./Section";
-import { socials } from "../constants";
 
 const Footer = () => {
-  return (
-    <>
-      <Section crosses className="!px-0 !py-10">
-        <div className="container flex sm:justify-between items-center gap-10 max-sm:flex-col">
-          <p className="caption text-n-4 lg:block">
-            © {new Date().getFullYear()} All rights reserve.
-          </p>
-
-          <ul className="flex gap-5 flex-wrap">
-            {socials.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                target="_blank"
-                className="flex items-center justify-center w-10 h-10 rounded-full bg-n-7 transition-colors hover:bg-n-6"
-              >
-                <img
-                  src={item.iconUrl}
-                  width={16}
-                  height={16}
-                  alt={item.title}
-                />
-              </a>
-            ))}
-          </ul>
-        </div>
-      </Section>
-    </>
-  );
+  return <div></div>;
 };
 
 export default Footer;
